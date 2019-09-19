6da539674d8d183d3b5337860cd3130ded231814
EricJeffrey <1719937412@qq.com>
2019-09-19 09:14:35 +0000

    add uid/gid map for simp_container

--- simple_container/simp_container.cpp
+++ simple_container/simp_container.cpp
@@ -1,4 +1,5 @@
 #include "/home/eric/coding/my_lib/common.h"
+#include <dirent.h>
 #include <fcntl.h>
 #include <sched.h>
 #include <signal.h>
@@ -14,10 +15,17 @@
 #include <unistd.h>
 #include <wordexp.h>
 
-#define logger(A)                      \
-    do {                               \
-        fprintf(stdout, "%s...\n", A); \
-        fflush(stdout);                \
+const int STACK_SIZE = 1024 * 1024;
+char child_stack[STACK_SIZE];
+
+#define LOG_ON 1
+
+#define LOGGER(A)                          \
+    do {                                   \
+        if (LOG_ON) {                      \
+            fprintf(stderr, "%s...\n", A); \
+            fflush(stderr);                \
+        }                                  \
     } while (0)
 
 #define md(A) mkdir(A, S_IRWXU | S_IRWXG)
@@ -41,22 +49,22 @@ void cpy_file(const char old_path[], const char new_path[]) {
 
 // make needed dirs, this will move cwd to simp_contianer_root
 void prepare_dirs() {
-    logger("preparing dirs");
-    // md("/data");
-    // chown("/data", 1000, 1000);
+    LOGGER("PREPARE DIRS");
+
     md("./simp_container_root/");
     chdir("./simp_container_root/");
     md("./new_root");
     md("./new_root/bin");
-    md("./new_root/data");
     md("./new_root/proc");
     md("./new_root/old_root");
 }
 
 // execute busybox inside container
 void exec_cmd() {
-    int ret = 0;
-    if ((ret = fork()) > 0) { // parent
+    LOGGER("EXEC CMD");
+
+    int ret = fork();
+    if (ret > 0) { // parent
         waitpid(ret, NULL, 0);
         execl("/bin/sh", "sh", NULL);
         errExit("execl");
@@ -68,58 +76,118 @@ void exec_cmd() {
     errExit("fork");
 }
 
-// unshare and fork
-void do_unshare() {
-    logger("doing unshare");
-    int ret = 0;
-    int us_flags = CLONE_NEWCGROUP | CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWUSER | CLONE_NEWUTS;
-    ret = unshare(us_flags);
+// new namespace init
+int new_ns_init(void *arg) {
+    // sync with map_user_grp
+    int *pipe_fd = (int *)arg;
+    close(pipe_fd[1]);
+    char ch;
+    // block untile fd[1] is closed
+    if (read(pipe_fd[0], &ch, 1) != 0)
+        errExit("PIPE READ ERROR!");
+
+    LOGGER("NAMESPACE INIT");
+
+    const char hostname[] = "container01";
+
+    LOGGER("SET HOSTNAME");
+    sethostname(hostname, strlen(hostname));
+
+    LOGGER("chdir to new root");
+    mount("./new_root", "./new_root", NULL, MS_BIND, NULL);
+    chdir("./new_root");
+
+    LOGGER("pivot root");
+    syscall(SYS_pivot_root, "./", "./old_root");
+
+    LOGGER("doing mount and umount");
+    mount("none", "/proc", "proc", 0, NULL);
+    umount("/old_root");
+
+    LOGGER("exec busybox sh");
+    exec_cmd();
+}
+
+// update uid/gid map file
+void update_map(char *map_file_path, char const *content) {
+    int fd_uid = open(map_file_path, O_RDWR);
+    int len = strlen(content);
+    if (fd_uid < 0)
+        errExit("OPEN %s ERROR!", map_file_path);
+    int ret = write(fd_uid, content, len);
     if (ret < 0)
-        errExit("unshare");
-    if ((ret = fork()) < 0)
-        errExit("fork");
-    if (ret == 0) { // child
-        const char hostname[] = "container01";
-        logger("setting hostname");
-        sethostname(hostname, strlen(hostname));
-        logger("chdir to new root");
-        mount("./new_root", "./new_root", NULL, MS_BIND, NULL);
-        chdir("./new_root");
-        logger("pivot root");
-        syscall(SYS_pivot_root, "./", "./old_root");
-        logger("doing mount and umount");
-        mount("none", "/proc", "proc", 0, NULL);
-        umount("/old_root");
-        logger("set env and exec busybox sh");
-        setenv("hostname", "container01", 0);
-        // setenv("PS1", "root@$container01:$(pwd)# ", 0);
-        exec_cmd();
-    }
-    if (ret > 0) { // parent
-        waitpid(ret, NULL, 0);
-        exit(0);
-    }
+        errExit("WRITE %s ERROR!", map_file_path);
+    close(fd_uid);
+}
+
+// map uid and gid, 0 1000 10
+void map_ugid(pid_t child_pid) {
+    LOGGER("MAP UID GID");
+
+    const int buf_size = 30;
+    char buf[] = "0 1000 1";
+    char id_path[buf_size] = {};
+
+    snprintf(id_path, buf_size, "/proc/%ld/uid_map", (long)child_pid);
+    update_map(id_path, "0 1000 1");
+
+    // write deny to /proc/$$/setgroups
+    char setgrp_path[buf_size] = {};
+    snprintf(setgrp_path, buf_size, "/proc/%ld/setgroups", (long)child_pid);
+    update_map(setgrp_path, "deny");
+
+    memset(id_path, 0, strlen(id_path));
+    snprintf(id_path, buf_size, "/proc/%ld/gid_map", (long)child_pid);
+    update_map(id_path, buf);
+}
+
+// unshare and fork
+int do_clone(int pipe_fd[2]) {
+    LOGGER("DO CLONE");
+
+    int ns_flag = 0;
+    // ns_flag |= CLONE_NEWCGROUP;
+    ns_flag |= CLONE_NEWIPC;
+    ns_flag |= CLONE_NEWNET;
+    ns_flag |= CLONE_NEWNS;
+    ns_flag |= CLONE_NEWPID;
+    ns_flag |= CLONE_NEWUTS;
+    ns_flag |= CLONE_NEWUSER;
+    ns_flag |= SIGCHLD;
+
+    int ret = clone(new_ns_init, child_stack + STACK_SIZE, ns_flag, pipe_fd);
+    if (ret < 0)
+        errExit("CLONE ERROR!");
+    return ret;
 }
 
 // make root dir and move program to root/bin
 void create_container() {
-    logger("preparing root");
-    prepare_dirs();
-    // copy file
-    logger("copying file");
-    cpy_file("/home/eric/coding/busybox", "./new_root/bin/busybox");
-    do_unshare();
-    errExit("do unshare");
+    LOGGER("CREATE CONTAINER");
 
-    // stop here to test
-    // logger("execl busybox sh");
-    // int ret = execl("./busybox", "busybox", "sh", NULL);
-    // errExit("execl");
-    // exit(0);
+    prepare_dirs();
+
+    cpy_file("/home/eric/coding/busybox", "./new_root/bin/busybox");
+
+    int pipe_fd[2];
+    pipe(pipe_fd);
+
+    pid_t child_pid;
+    child_pid = do_clone(pipe_fd);
+
+    map_ugid(child_pid);
+    close(pipe_fd[1]);
+    LOGGER("PARENT: CLOSE PIPE");
+
+    if (child_pid > 0) { // parent
+        waitpid(child_pid, NULL, 0);
+        exit(0);
+    }
+
+    errExit("CREATE CONTAINER ERROR");
 }
 
 int main(int argc, char const *argv[]) {
-    logger("Running...");
     create_container();
-    // create_container();
+    return 0;
 }
