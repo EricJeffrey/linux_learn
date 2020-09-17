0afd36b8a386c878398bb19b309837ce8fb8809a
EricJeffrey <1719937412@qq.com>
2020-09-17 15:05:08 +0800

    add iptables config code

--- net_manage_exec/main.cpp
+++ net_manage_exec/main.cpp
@@ -29,12 +29,45 @@ using std::vector;
 1. ip link add br-microc type bridge
 2. ip address add 10.0.1.1/24 dev br-microc
 3. ip link set br-microc up
+4. iptables -t nat -A POSTROUTING -s 10.0.1.0/24 ! -d 10.0.1.0/24 -j MASQUERADE
+
+
+------------------- some infomation -------------------
+
+Chain POSTROUTING (policy ACCEPT)
+target      prot opt source              destination
+CNI-4       all  --  10.88.0.15           0.0.0.0/0
+MASQUERADE  all  --  10.0.1.0/24         !10.0.1.0/24
+MASQUERADE  all  --  0.0.0.0/0           !10.0.1.0/24
+
+Chain CNI-4 (1 references)
+target     prot opt source               destination
+ACCEPT     all  --  0.0.0.0/0            10.88.0.0/16
+MASQUERADE  all  --  0.0.0.0/0           !224.0.0.0/4
+
+
+// bridge -> other
+iptables -t nat -A POSTROUTING -s 10.0.1.0/24 ! -d 10.0.1.0/24 -j MASQUERADE
+// other -> bridge
+iptables -t nat -A POSTROUTING -d 10.0.1.0/24 -j ACCEPT
+
+// create veth
+ip link add veth233 type veth peer name veth322
+ip link set veth233 master br-microc
+ip address add 10.0.1.14/24 dev veth322
+ip link set veth233 up
+ip link set veth322 up
 
  */
 
 const string bridgeName = "br-microc";
+const string bridgeAddr = "10.0.1.1/24";
+const string bridgeSubnetAddr = "10.0.1.0/24";
+
 static const char ipPath[] = "/usr/sbin/ip";
 static const char ipName[] = "ip";
+static const char iptablesPath[] = "/usr/sbin/iptables";
+static const char iptablesName[] = "iptables";
 
 class IncorrectlyExitError : public runtime_error {
 public:
@@ -85,6 +118,31 @@ int fork_exec_wait(const string &filePath, const vector<string> &args, bool noOu
     return 0;
 }
 
+// configure nat if not exist, throw on error
+void confBridgeNat() {
+    vector<string> checkArgv({iptablesName, "-t", "nat", "-C", "POSTROUTING", "-s",
+                              bridgeSubnetAddr, "!", "-d", bridgeSubnetAddr, "-j", "MASQUERADE"});
+    vector<string> addArgv({iptablesName, "-t", "nat", "-A", "POSTROUTING", "-s", bridgeSubnetAddr,
+                            "!", "-d", bridgeSubnetAddr, "-j", "MASQUERADE"});
+    int retCode = fork_exec_wait(iptablesPath, checkArgv, true);
+    if (retCode == 0) {
+        return;
+    } else {
+        retCode = fork_exec_wait(iptablesPath, addArgv, true);
+        if (retCode != 0)
+            throw runtime_error("Add iptables rule failed: " + concat(addArgv));
+    }
+}
+
+// throw on error
+void deleteBridgeNat() {
+    vector<string> delArgv({iptablesName, "-t", "nat", "-D", "POSTROUTING", "-s", bridgeSubnetAddr,
+                            "!", "-d", bridgeSubnetAddr, "-j", "MASQUERADE"});
+    int retCode = fork_exec_wait(iptablesPath, delArgv);
+    if (retCode != 0)
+        throw runtime_error("Delete iptables rule failed: " + concat(delArgv));
+}
+
 // throw on error
 void deleteBridgeDev() {
     vector<string> argv = {ipName, "link", "delete", bridgeName};
@@ -103,7 +161,7 @@ bool checkBridgeExits() {
 void createBridgeDev() {
     vector<vector<string>> argvs = {
         {ipName, "link", "add", bridgeName, "type", "bridge"},
-        {ipName, "address", "add", "10.0.1.1/24", "dev", bridgeName},
+        {ipName, "address", "add", bridgeAddr, "dev", bridgeName},
         {ipName, "link", "set", bridgeName, "up"},
     };
     for (auto &&argv : argvs) {
@@ -113,22 +171,26 @@ void createBridgeDev() {
     }
 }
 
+// create bridge and config nat (via iptable)
 // no throw
 void createBridge() {
     try {
         if (!checkBridgeExits()) {
-            cout << "Creating bridge device" << endl;
+            cout << "Creating bridge" << endl;
             createBridgeDev();
+            confBridgeNat();
+            cout << "Created bridge" << endl;
         }
     } catch (const std::exception &e) {
-        cerr << "Create bridge device failed: " << e.what() << endl;
+        cerr << "Create bridge failed: " << e.what() << endl;
         try {
             if (checkBridgeExits()) {
-                cout << "Deleting bridge device" << endl;
+                cout << "Deleting bridge" << endl;
                 deleteBridgeDev();
+                deleteBridgeNat();
             }
         } catch (const std::exception &e) {
-            cerr << "Delete bridge device failed: " << e.what() << endl;
+            cerr << "Delete bridge failed: " << e.what() << endl;
         }
     }
 }
